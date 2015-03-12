5f5e633f29cad34bed99627b3169f50e79589409
Benjamin Schuedzig <benjamin.schuedzig@mantro.net>
2015-03-12 20:55:24 +0100

    Added support for windows 7 (local GPO)

--- powershell/setup-defender.ps1
+++ powershell/setup-defender.ps1
@@ -16,6 +16,28 @@ Param(
 $tmpPath = $path
 $tmpPath += "\tmp"
 
+function Add-WindowsDefenderExclusionsPolicy([string]$pathToAdd)
+{                  
+    $registryKey = "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Exclusions\Paths" 
+       
+    $found = $False
+    
+    $items = Get-Item $registryKey
+        
+    Foreach ($name in $items.GetValueNames()) {
+        if ([string]::Compare($name, $pathToAdd, $True) -eq 0) {
+            # Path already added
+            Write-Host "Path already added to exclusion list"
+            return
+        }
+    }
+            
+    Set-ItemProperty -Path $registryKey -Name $pathToAdd -Value 0 -Force
+    Write-Host "Path added to defender exclusion list, updating policy"
+            
+    gpupdate | Out-Null
+}
+
 function IsAdministrator
 {
     $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -48,12 +70,26 @@ if (!(IsAdministrator))
     }
 }
 
+
+
 # Set preference
 "Removing " + $path + " from Windows Defender's Eye"
-if (Get-Command Set-MpPreference -errorAction SilentlyContinue) {
+
+
+# https://msdn.microsoft.com/en-us/library/windows/desktop/ms724832%28v=vs.85%29.aspx
+$version = [Environment]::OSVersion.Version
+
+if ($version.Major -eq 6 -AND $version.Minor -gt 1) {
+  # Windows 8 and above
+  if (Get-Command Set-MpPreference -errorAction SilentlyContinue) {
     Set-MpPreference -ExclusionPath $path
-} else {
+  } else {
     "Defender Configuration not available, fallback required"
+  }  
+}
+elseif ($version.Major -eq 6 -AND $version.Minor -eq 1) {
+  # Windows 7 / Server 2008 R2
+  Add-WindowsDefenderExclusionsPolicy $path
 }
 
 "Done"
